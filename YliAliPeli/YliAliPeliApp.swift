775031ddac103b7ef5d30abe99ec758d61e88c2f
aliisaro <aliisarokala@metropolia.fi>
2025-01-31 14:19:35 +0200

    Initial Commit

---
+++ YliAliPeli/YliAliPeliApp.swift
@@ -0,0 +1,17 @@
+//
+//  YliAliPeliApp.swift
+//  YliAliPeli
+//
+//  Created by Aliisa Rokala on 31.1.2025.
+//
+
+import SwiftUI
+
+@main
+struct YliAliPeliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YliAliPeliTests/YliAliPeliTests.swift
@@ -0,0 +1,17 @@
+//
+//  YliAliPeliTests.swift
+//  YliAliPeliTests
+//
+//  Created by Aliisa Rokala on 31.1.2025.
+//
+
+import Testing
+@testable import YliAliPeli
+
+struct YliAliPeliTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
