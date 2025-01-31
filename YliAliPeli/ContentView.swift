cbebb915bf62e65dd9903f2880c3286c9d92bb47
aliisaro <aliisarokala@metropolia.fi>
2025-01-31 14:54:43 +0200

    initial commit

--- YliAliPeli/ContentView.swift
+++ YliAliPeli/ContentView.swift
@@ -1,19 +1,51 @@
-//
-//  ContentView.swift
-//  YliAliPeli
-//
-//  Created by Aliisa Rokala on 31.1.2025.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject private var game = YliAli(low: 1, high: 100)
+    @State private var arvaukset: [Int] = []
+    @State private var arvaus: String = ""
+    @State private var tulos: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            // Arvaukset
+            Text("Arvaukset: \(arvaukset.map { String($0) }.joined(separator: ", "))")
+                .foregroundColor(.pink)
+                .padding()
+            
+            // Tulos viesti
+            Text(tulos)
+                .foregroundColor(.blue)
+                .padding()
+            
+            // Tekstikenttä arvaukselle
+            TextField("Arvaa luku", text: $arvaus)
+                .keyboardType(.numberPad)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+
+            // Arvaa nappi
+            Button("Arvaa") {
+                if let guess = Int(arvaus) {
+                    let result = game.arvaa(arvaus: guess)
+                    arvaukset.append(guess)
+                    
+                    switch result {
+                        case .Ali:
+                            tulos = "Liian matala"
+                        case .Yli:
+                            tulos = "Liian korkea"
+                        case .Osuma:
+                            tulos = "Oikein!"
+                    }
+                    arvaus = "" // Resetoidaan tesktikenttä
+                } else {
+                    tulos = "Anna kelvollinen numero"
+                }
+            }
+            .foregroundColor(.black)
+            
         }
         .padding()
     }
---
+++ YliAliPeli/YliAliPeli.swift
@@ -0,0 +1,41 @@
+//
+//  YliAliPeli.swift
+//  YliAliPeli
+//
+//  Created by Aliisa Rokala on 31.1.2025.
+//
+import SwiftUI
+
+enum PeliTulos {
+    case Ali, Yli, Osuma
+}
+class YliAli: ObservableObject {
+    let secret: Int
+    let low: Int
+    let high: Int
+    
+    @Published private(set) var guesses:
+Array<Int> = []
+    init(low: Int, high: Int) {
+    self.low = low
+    self.high = high
+    self.secret =
+(low...high).randomElement() ?? low
+}
+    
+func arvaa(arvaus: Int) ->
+PeliTulos {
+    guesses.append(arvaus)
+    if arvaus < secret {
+        return PeliTulos.Ali
+    } else if arvaus > secret {
+        return PeliTulos.Yli
+    } else {
+        return PeliTulos.Osuma
+    }
+}
+    
+    func tries() -> Int {
+        return guesses.count
+    }
+}
